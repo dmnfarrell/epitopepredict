45d3fcb7dddcfdd6cb45441a6019851326b9ac17
Damien Farrell <farrell.damien@gmail.com>
2016-04-05 12:37:49 +0100

    minor

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
-
 from setuptools import setup
 import sys,os
-home=os.path.expanduser('~')
 
 setup(
     name = 'epitopepredict',
@@ -12,7 +10,8 @@ setup(
     author = 'Damien Farrell',
     author_email = 'farrell.damien[at]gmail.com',
     packages = ['epitopepredict'],
-    package_data={'epitopepredict': ['mhcdata/*.csv','tepitope/*.txt','tepitope/pssm/*']},
+    package_data={'epitopepredict': ['mhcdata/*.csv','tepitope/*.txt',
+				     'tepitope/pssm/*']},
     install_requires=['numpy>=1.5',
                       'pandas>=0.17',
                       'biopython>=1.5'],
@@ -22,7 +21,7 @@ setup(
             'Operating System :: MacOS :: MacOS X',
             'Operating System :: Microsoft :: Windows',
             'Operating System :: POSIX',
-            'License :: OSI Approved :: Apache License v2',
+            'License :: OSI Approved :: Apache Software License',
             'Development Status :: 4 - Beta',
             'Intended Audience :: Science/Research'],
     keywords = ['epitope','mhc','immunology','biology'],
