8db901125a6524142b236e155e9ca68cd91ada20
Damien Farrell <farrell.damien@gmail.com>
2017-10-09 20:51:30 +0100

    moved app methods to web module

--- epitopepredict/tornado_serve.py
+++ epitopepredict/tornado_serve.py
@@ -9,7 +9,7 @@
 import sys,os,glob
 import pandas as pd
 import numpy as np
-from epitopepredict import base, plotting
+from epitopepredict import base, web
 
 import tornado.ioloop
 import tornado.web
@@ -17,93 +17,12 @@ from wtforms_tornado import Form
 from wtforms import TextField, StringField, SelectField, FloatField
 from tornado.web import RequestHandler
 from bokeh.util.browser import view
-from bokeh.models import ColumnDataSource, Slider
 from bokeh.plotting import figure
-from bokeh.layouts import row, column, gridplot, widgetbox
+from bokeh.layouts import row, column, gridplot, widgetbox, layout
 from bokeh.embed import components
 
 path = 'results'
-predictors = base.predictors
-wikipage = 'https://github.com/dmnfarrell/epitopepredict/wiki'
 plotkinds = ['tracks','bar','text']
-
-def get_file_lists(path):
-
-    names = []
-    for p in predictors:
-        files = glob.glob(os.path.join(path, p, '*.csv'))
-        n = [os.path.splitext(os.path.basename(i))[0] for i in files]
-        names.extend(n)
-    names = set(names)
-    names = sorted(names)
-    return names
-
-def get_results(path, predictor, name):
-
-    filename = os.path.join(path, predictor, name)
-    P = base.get_predictor(predictor)
-    P.load(filename+'.csv')
-    #print filename
-    #print P.data
-    return P
-
-def get_seq_info(P):
-    df = P.data
-    l = base.get_length(df)
-    seq = sequence_from_peptides(df)
-    return {'n-mer':l, 'sequence':seq}
-
-def sequence_from_peptides(df):
-    x = df.peptide.str[0]
-    x = ''.join(x)
-    return x
-
-def get_predictors(path, name):
-    """Get a set of predictors with available results"""
-
-    if name==None:
-        return []
-    preds = []
-    for pred in predictors:
-        P = get_results(path, pred, name)
-        preds.append(P)
-    return preds
-
-def create_figures(preds, name='', kind='tracks', cutoff=5, n=2):
-    """Get plots of binders for single protein/sequence"""
-
-    figures = []
-    if kind == 'tracks':
-        plot = plotting.bokeh_plot_tracks(preds, title=name,
-                         width=800, palette='Set1', cutoff=float(cutoff), n=int(n))
-
-    elif kind == 'bar':
-        plot = plotting.bokeh_plot_bar(preds, title=name)
-    if plot is not None:
-        figures.append(plot)
-    return figures
-
-def create_pred_table(path, name):
-    """Create table of prediction data"""
-
-    P = get_results(path, 'tepitope', name)
-    df = P.data[:10]
-    data = dict(
-        peptide=df.peptide.values,
-        pos=df.pos.values,
-        score=df.score.values,
-        allele=df.allele.values
-    )
-    #print (df)
-    source = ColumnDataSource(data)
-    columns = [
-            TableColumn(field="peptide", title="peptide"),
-            TableColumn(field="pos", title="pos"),
-            TableColumn(field="score", title="score"),
-            TableColumn(field="allele", title="allele"),
-        ]
-    table = DataTable(source=source, columns=columns, width=400, height=280)
-    return table
 
 def help_msg():
     msg = '<a>results path not found, enter a folder with your results</a><br>'
@@ -126,38 +45,41 @@ class MainHandler(RequestHandler):
         args = self.request.arguments
         form = ControlsForm()
         print args
-        if 'path' in args:
-           path = args['path'][0]
-        else:
-            path = 'results'
+        defaultargs = {'path':'results','name':'','cutoff':5,'n':2,'kind':'tracks'}
+        for k in defaultargs:
+            if k in args:
+                defaultargs[k] = args[k][0]
+        path = defaultargs['path']
+        current_name = defaultargs['name']
+
         if not os.path.exists(path):
             msg = help_msg()
             self.render('index.html', script='', div='', form=form, msg=msg)
             return
-        if 'name' in args:
-            current_name = args['name'][0]
-        else:
-            current_name = 'Rv0011c'
-        cutoff=5
-        n=2
-        kind='tracks'
-        names = get_file_lists(path)
 
+        names = web.get_file_lists(path)
+        if current_name == '': current_name = names[0]
         form.path.data = path
         form.name.choices = [(i,i) for i in names]
         form.name.data = current_name
+        form.cutoff.data = defaultargs['cutoff']
+        form.n.data = defaultargs['n']
+        form.kind.data = defaultargs['kind']
 
-        preds = get_predictors(path, current_name)
-        plots = create_figures(preds, current_name, cutoff=cutoff, n=n, kind=kind)
+        preds = web.get_predictors(path, current_name)
+        plots = web.create_figures(preds, **defaultargs)
+        table = web.create_pred_table(path, current_name)
         #info = get_seq_info(preds[0])['sequence']
 
         if len(plots) > 0:
             grid = gridplot(plots, ncols=1, merge_tools=True, #sizing_mode='stretch_both',
                             toolbar_options=dict(logo=None))
+        else:
+            grid = ''
+
         script, div = components(grid)
 
-        text = '<h4> using tornado.... </h4>'
-        self.render('index.html', script=script, div=div, form=form, msg='')
+        self.render('index.html', script=script, div=div, form=form, msg='', info='')
 
     @staticmethod
     def modify_doc(doc):
---
+++ epitopepredict/web.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+"""
+    epitopepredict, methods for supporting web app
+    Created Sep 2017
+    Copyright (C) Damien Farrell
+"""
+
+from __future__ import absolute_import, print_function
+import sys,os,glob
+import pandas as pd
+import numpy as np
+from . import base, plotting, sequtils
+from bokeh.models import ColumnDataSource, Slider
+from bokeh.models.widgets import DataTable, TableColumn, Select, Button, Slider, TextInput
+from bokeh.layouts import row, column, gridplot, widgetbox, layout
+from bokeh.embed import components
+
+path = 'results'
+predictors = base.predictors
+wikipage = 'https://github.com/dmnfarrell/epitopepredict/wiki'
+plotkinds = ['tracks','bar','text']
+
+def get_file_lists(path):
+
+    names = []
+    for p in predictors:
+        files = glob.glob(os.path.join(path, p, '*.csv'))
+        n = [os.path.splitext(os.path.basename(i))[0] for i in files]
+        names.extend(n)
+    names = set(names)
+    names = sorted(names)
+    return names
+
+def get_results(path, predictor, name):
+
+    filename = os.path.join(path, predictor, name)
+    P = base.get_predictor(predictor)
+    P.load(filename+'.csv')
+    #print filename
+    #print P.data
+    return P
+
+def get_seq_info(P):
+    df = P.data
+    if df is None:
+        return ''
+    df = df.drop_duplicates('pos').sort_values('pos')
+    l = base.get_length(df)
+    seq = sequence_from_peptides(df)
+    return {'n-mer':l, 'sequence':seq}
+
+def sequence_from_peptides(df):
+    x = df.peptide.str[0]
+    x = ''.join(x)
+    return x
+
+def get_predictors(path, name):
+    """Get a set of predictors with available results"""
+
+    if name==None:
+        return []
+    preds = []
+    for pred in predictors:
+        P = get_results(path, pred, name)
+        preds.append(P)
+    return preds
+
+def create_figures(preds, name='', kind='tracks', cutoff=5, n=2, **kwargs):
+    """Get plots of binders for single protein/sequence"""
+
+    figures = []
+    if kind == 'tracks':
+        plot = plotting.bokeh_plot_tracks(preds, title=name,
+                         width=800, palette='Set1', cutoff=float(cutoff), n=int(n))
+
+    elif kind == 'bar':
+        plot = plotting.bokeh_plot_bar(preds, title=name, width=800 )
+    if plot is not None:
+        figures.append(plot)
+    return figures
+
+def create_pred_table(path, name):
+    """Create table of prediction data"""
+
+    P = get_results(path, 'tepitope', name)
+    df = P.data[:10]
+    data = dict(
+        peptide=df.peptide.values,
+        pos=df.pos.values,
+        score=df.score.values,
+        allele=df.allele.values
+    )
+    #print (df)
+    source = ColumnDataSource(data)
+    columns = [
+            TableColumn(field="peptide", title="peptide"),
+            TableColumn(field="pos", title="pos"),
+            TableColumn(field="score", title="score"),
+            TableColumn(field="allele", title="allele"),
+        ]
+    table = DataTable(source=source, columns=columns, width=400, height=280)
+
+    return table
+
+def create_widgets():
+
+    select = Select(title="Name:", value="name", options=["foo", "bar"])
+    slider = Slider(start=0, end=100, value=5, step=.5, title="Cutoff")
+    button = Button(label="Submit", button_type="success")
+    #text_input = TextInput(value="default", title="Name:")
+    return widgetbox([button,select,slider], width=200)
+
+def test():
+    from bokeh.io import output_file, show
+    path='results'
+    name='Rv0011c'
+    preds = get_predictors(path, name)
+    plots = create_figures(preds)
+    table = create_pred_table(path, name)
+    grid = gridplot(plots, ncols=1, merge_tools=True)
+    widgets = create_widgets()
+    l = layout([[ plots, widgets ]], ncols=2, nrows=1)
+    #script, div = components(l)
+    show(l)
+
+if __name__ == "__main__":
+    test()
